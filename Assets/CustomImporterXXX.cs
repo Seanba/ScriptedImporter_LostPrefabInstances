d2e4b7093ca40be76ea6d88c26f1acb814994d01
Seanba <sean@seanba.com>
2021-01-04 15:47:50 -0600

    Add custom importer and cube object to sample scene

---
+++ Assets/CustomImporterXXX.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor.Experimental.AssetImporters;
+
+[ScriptedImporter(1, "xxx")]
+public class CustomImporterXXX : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+        // Simply create a cube object for this prefab
+        var cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        ctx.AddObjectToAsset("main obj", cube);
+        ctx.SetMainObject(cube);
+    }
+
+}
